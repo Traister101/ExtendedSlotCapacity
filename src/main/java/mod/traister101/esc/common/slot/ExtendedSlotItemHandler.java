a5a7530b2049ec3ccd28a7e4f0d1c2be7d491a67
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-11-05 04:01:54 -0800

    We should call `IItemHandler#getSlotLimit` instead of `Slot#getMaxStackSize`

--- src/main/java/mod/traister101/esc/common/slot/ExtendedSlotItemHandler.java
+++ src/main/java/mod/traister101/esc/common/slot/ExtendedSlotItemHandler.java
@@ -3,26 +3,26 @@ package mod.traister101.esc.common.slot;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 
-import net.minecraftforge.items.IItemHandler;
-import net.minecraftforge.items.IItemHandlerModifiable;
-import net.minecraftforge.items.SlotItemHandler;
+import net.minecraftforge.items.*;
 
 /**
  * Same as Forges {@link SlotItemHandler} but with an overridden {@link Slot#getMaxStackSize(ItemStack)} which respects the extended slot count
  */
 public class ExtendedSlotItemHandler extends SlotItemHandler {
 
+	public final int slotIndex;
+
 	public ExtendedSlotItemHandler(final IItemHandler itemHandler, final int slotIndex, final int xPosition, final int yPosition) {
 		super(itemHandler, slotIndex, xPosition, yPosition);
+		this.slotIndex = slotIndex;
 	}
 
 	@Override
 	public int getMaxStackSize(final ItemStack itemStack) {
-		final int maxInput = getMaxStackSize();
+		final IItemHandler handler = getItemHandler();
+		final int maxInput = handler.getSlotLimit(slotIndex);
 		final ItemStack maxAdd = itemStack.copyWithCount(maxInput);
 
-		final IItemHandler handler = getItemHandler();
-		final int slotIndex = getSlotIndex();
 		final ItemStack currentStack = handler.getStackInSlot(slotIndex);
 
 		if (handler instanceof final IItemHandlerModifiable handlerModifiable) {
